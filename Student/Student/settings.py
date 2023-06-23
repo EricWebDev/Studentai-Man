0a0fea29849d55734453d32a7320cb83c7ff3617
EricWebDev <erikasj1999@gmail.com>
2023-06-23 10:30:55 +0300

    local

--- Student/Student/settings.py
+++ Student/Student/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from Student.Student import local_settings
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zo!4xtggmfe*a7un6no31n+s9kiriojb*vxthy%*#5^*m5zoys'
-
+SECRET_KEY = local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
